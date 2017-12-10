b17b0bc18c64d86be8acc43419ce3ad967853e19
dubosew <wmdubose@gmail.com>
2017-12-10 07:05:26 -0600

    12/4/2017 link adjustments 12/10/2017 Base Complete

---
+++ myeccapp/src/main/java/chatapp/InstantMessage.java
@@ -0,0 +1,27 @@
+package chatapp;
+
+/**
+ * Created by dubosew on 12/9/2017.
+ */
+
+public class InstantMessage {
+
+    private String message;
+    private String author;
+
+    public InstantMessage(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public InstantMessage() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
